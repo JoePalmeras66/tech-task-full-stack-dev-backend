9d06db7e0643966b31c5d8f8589d02c2a1cdc315
Joe Palmeras <joepalmeras-github@web.de>
2022-12-18 20:56:46 +0100

    TASK-5 Change 'Logger' in 'RandomUserConig'

--- tech-task-dev-backend/src/main/java/org/demicon/tech/task/d3/cloud/config/RandomUserConfig.java
+++ tech-task-dev-backend/src/main/java/org/demicon/tech/task/d3/cloud/config/RandomUserConfig.java
@@ -1,13 +1,13 @@
 package org.demicon.tech.task.d3.cloud.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.logging.Logger;
 
 @EnableAsync
 @EnableScheduling
@@ -16,7 +16,7 @@ public class RandomUserConfig {
     @Value("${config.endpoint.url}")
     private String url;
 
-    static final Logger LOG = Logger.getLogger(RandomUserConfig.class.getName());
+    static final Logger LOG = LoggerFactory.getLogger(RandomUserConfig.class);
 
     @Bean
     WebClient createWebClient() {
