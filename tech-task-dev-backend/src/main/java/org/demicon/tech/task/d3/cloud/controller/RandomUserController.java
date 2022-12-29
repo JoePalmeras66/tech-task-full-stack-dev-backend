551333cd8235c993ddb12fc954ddfc345e7c3d51
Joe Palmeras <joepalmeras-github@web.de>
2022-12-29 15:17:33 +0100

    TASK-21 Implement Swagger/OpenAPI-Interfaces for 'RandomUserController'

--- tech-task-dev-backend/src/main/java/org/demicon/tech/task/d3/cloud/controller/RandomUserController.java
+++ tech-task-dev-backend/src/main/java/org/demicon/tech/task/d3/cloud/controller/RandomUserController.java
@@ -4,47 +4,52 @@ import lombok.RequiredArgsConstructor;
 import org.demicon.tech.task.d3.cloud.converter.toresponse.RandomDataStreamEntityToResponseConverter;
 import org.demicon.tech.task.d3.cloud.converter.toresponse.RandomDataPageEntityToResponseConverter;
 import org.demicon.tech.task.d3.cloud.service.impl.RandomUserServiceImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import tech.task.d3.cloud.api.RandomusersApi;
 import tech.task.d3.cloud.api.model.PageableRandomUserDTO;
 import tech.task.d3.cloud.api.model.RandomUserDTO;
 
 import org.springframework.http.ResponseEntity;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Stream;
 
-@CrossOrigin
-@RestController
-@RequestMapping("/techtask/api/v1")
+import javax.validation.Valid;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
-public class RandomUserController {
+public class RandomUserController implements RandomusersApi {
     private final RandomUserServiceImpl randomUserService;
     private final RandomDataStreamEntityToResponseConverter randomDataStreamEntityToResponseConverter;
     private final RandomDataPageEntityToResponseConverter randomDataPageEntityToResponseConverter;
 
-    @GetMapping("/randomusers/all")
-    public CompletableFuture<ResponseEntity<PageableRandomUserDTO>> findAllByLocationCountryPagination(
-            @RequestParam("country") String country,
-            @RequestParam("state") String state,
-            @RequestParam("city") String city,
-            @RequestParam("gender") String gender,
-            @PageableDefault(sort = "createdOn") Pageable pageable) {
-
+    @Override
+    public CompletableFuture<ResponseEntity<PageableRandomUserDTO>> getRandomUsers(
+            @Valid String country,
+            @Valid String state,
+            @Valid String city,
+            @Valid String gender,
+            @Valid Integer page,
+            @Valid Integer size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.Direction.DESC, "createdOn");
         return CompletableFuture.completedFuture(new ResponseEntity<>(this.randomDataPageEntityToResponseConverter.convert(
                 this.randomUserService.findAll(country, state, city, gender, pageable)), HttpStatus.OK)
         );
     }
 
-    @GetMapping("/randomusers/all/stream")
-    public CompletableFuture<ResponseEntity<Stream<RandomUserDTO>>> findAllByLocationCountry(
-            @RequestParam("country") String country,
-            @RequestParam("state") String state,
-            @RequestParam("city") String city,
-            @RequestParam("gender") String gender) {
-        return CompletableFuture.completedFuture(new ResponseEntity<>(this.randomDataStreamEntityToResponseConverter.convert(
-                this.randomUserService.findAll(country, state, city, gender)), HttpStatus.OK)
+    @Override
+    public CompletableFuture<ResponseEntity<List<RandomUserDTO>>> getStreamRandomUsers(
+            @Valid String country,
+            @Valid String state,
+            @Valid String city,
+            @Valid String gender) {
+        return CompletableFuture.completedFuture(new ResponseEntity<>(
+                        this.randomDataStreamEntityToResponseConverter.convert(
+                                this.randomUserService.findAll(country, state, city, gender)
+                        ).collect(Collectors.toList()), HttpStatus.OK
+                )
         );
     }
 }
