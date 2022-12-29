f96f7b297392c4011f8b66009c957e25d66fae1f
Joe Palmeras <joepalmeras-github@web.de>
2022-12-29 20:02:02 +0100

    TASK-22 Fix 'NullPointerException' in 'RandomUserFilter' and 'LocationFilter'

--- tech-task-dev-backend/src/main/java/org/demicon/tech/task/d3/cloud/repository/filters/LocationFilter.java
+++ tech-task-dev-backend/src/main/java/org/demicon/tech/task/d3/cloud/repository/filters/LocationFilter.java
@@ -3,6 +3,7 @@ package org.demicon.tech.task.d3.cloud.repository.filters;
 import com.google.common.base.Strings;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
+import org.demicon.tech.task.d3.cloud.repository.filters.utils.BaseFilter;
 
 import static org.demicon.tech.task.d3.cloud.repository.LocationRepository.qLocation;
 
@@ -11,9 +12,9 @@ public class LocationFilter {
     public static BooleanBuilder filter(String country,
                                         String state,
                                         String city) {
-        Predicate predicateCountry = qLocation.country.toLowerCase().eq(country.toLowerCase());
-        Predicate predicateState = qLocation.state.toLowerCase().eq(state.toLowerCase());
-        Predicate predicateCity = qLocation.city.toLowerCase().eq(city.toLowerCase());
+        Predicate predicateCountry = Strings.isNullOrEmpty(country) ? null : qLocation.country.toLowerCase().eq(country.toLowerCase());
+        Predicate predicateState = Strings.isNullOrEmpty(state) ? null : qLocation.state.toLowerCase().eq(state.toLowerCase());
+        Predicate predicateCity = Strings.isNullOrEmpty(city) ? null : qLocation.city.toLowerCase().eq(city.toLowerCase());
 
         BooleanBuilder predicateBuilder = (Strings.isNullOrEmpty(country)) ? new BooleanBuilder()
                 : BaseFilter.filterBasePredicate(country, new BooleanBuilder(), predicateCountry);
